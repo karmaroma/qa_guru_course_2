f0fdb48c6b0314886b786ea79fb01a19cd461812
Roman Golovin <romagolovin@gmail.com>
2020-08-09 14:47:32 +0500

    2nd homework

---
+++ src/test/java/tests/Form.java
@@ -0,0 +1,25 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class Form {
+    @Test
+    void fillFormTest() {
+        // Открыть форму
+        open("http://suninjuly.github.io/simple_form_find_task.html");
+
+        // Нажать "Поехали"
+        $(byTagName("input")).sendKeys("Name");
+        $(byName("last_name")).sendKeys("Surname");
+        $(byClassName("city")).sendKeys("DefaultCity");
+        $(byId("country")).sendKeys("Russia");
+        $(byCssSelector("button.btn")).click();
+        sleep(2000);
+
+
+    }
+
+}
