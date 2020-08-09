e2a87f46b3612c85e2c2a989f944ad5e0415a7af
Vasenkov Stanislav <aanher@gmail.com>
2020-08-09 21:45:23 +0300

    Update Lepra.java
    Code review

--- src/test/java/tests/Lepra.java
+++ src/test/java/tests/Lepra.java
@@ -6,28 +6,20 @@ import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
 
-public class Lepra {
+
+public class LepraTests {
     @Test
     void lepraPayTest() {
-        // Открыть leprosorium
         open("https://leprosorium.ru");
 
-        // Нажать "Поехали"
-        $(".b-login-cruise_button").pressEnter();
-        // Ввести почту
-        $("#js-login-cruise-pay_field_input").sendKeys("123@ru.com");
-        sleep(500);
-        // Отметить чекбокс
-        $("#js-login-cruise-pay_agreement_input").click();
-        sleep(500);
-        // Выбрать платежную систему
-        $(".b-login-cruise-pay_method-img").click();
-        //Нажать оплатить
-        $("#js-login-cruise_go").click();
-        sleep(8000);
+        $(".b-login-cruise_button").pressEnter(); // Нажать "Поехали"
+        $("#js-login-cruise-pay_field_input").sendKeys("123@ru.com"); // Ввести почту
+        $("#js-login-cruise-pay_agreement_input").click(); // Отметить чекбокс
+        $(".b-login-cruise-pay_method-img").click(); // Выбрать платежную систему
+        $("#js-login-cruise_go").click(); //Нажать оплатить
 
-        //assert: произошел переход на платежную систему
-        $(".shopLogoNative").shouldBe(visible);
+        // Assert: произошел переход на платежную систему
+        $(".shopLogoNative").waitUntil(visible, 10000).shouldBe(visible);
     }
 
 }
